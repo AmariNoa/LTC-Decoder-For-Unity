a2cd549cad218ff91db99c372f0c544643362c35
天鈴のあ <amarinoa.v@gmail.com>
2025-07-08 17:04:19 +0900

    [Update] Implement a process that triggers a timeout and treats the LTC signal as stopped when no signal is received for a certain duration.

--- Runtime/Scripts/LtcMaster.cs
+++ Runtime/Scripts/LtcMaster.cs
@@ -9,14 +9,22 @@ namespace com.amari_noa.unity.ltc.decoder
     {
         // リストの上にある(index番号の小さい)ものが優先して処理される
         [SerializeField] private List<LtcReceiver> ltcReceivers;
+        // LTC信号が止まってからタイムアウト(停止扱いと)するまでの時間
+        [SerializeField] private float timeoutSeconds = 0.2f;
+        [Space]
         // タイムコードの更新時に通知を受け取るイベント関数
         [SerializeField] private UnityEvent<string, double> onTimeCodeUpdated;
+        // タイムコードの停止時に通知を受け取るイベント関数
+        [SerializeField] private UnityEvent onTimeCodeStopped;
+        // デバッグログの出力フラグ
         [SerializeField] private bool debugLog;
 
         public string TimeCode { get; private set; }
         public double TimeSeconds { get; private set;}
 
         private LtcReceiver _currentReceiver;
+        private float _timeoutTimer;
+        private bool _stopped;
 
 
         private bool TryGetTimeCode(out string timeCode, out double timeSeconds)
@@ -36,12 +44,14 @@ namespace com.amari_noa.unity.ltc.decoder
                 // 取得に成功した一番最初のレシーバーの値を使う
                 timeCode = receiver.TimeCode;
                 timeSeconds = receiver.TimeSeconds;
+
                 return true;
             }
 
             // デコードに成功した最後のタイムコードを返す(内部処理が壊れないようにするため)
             timeCode = TimeCode;
             timeSeconds = TimeSeconds;
+
             return false;
         }
 
@@ -60,22 +70,41 @@ namespace com.amari_noa.unity.ltc.decoder
         {
             if (!TryGetTimeCode(out var timeCode, out var timeSeconds))
             {
-                /*
-                if (debugLog)
-                {
-                    Debug.LogWarning("No valid ltc signal.");
-                }
-                */
-
+                // 信号停止中
                 TimeCode = timeCode;
                 TimeSeconds = timeSeconds;
+
+                // タイムアウト系の処理
+                if (!_stopped)
+                {
+                    // タイムアウトタイマー加算
+                    _timeoutTimer += Time.deltaTime;
+                    if (_timeoutTimer >= timeoutSeconds)
+                    {
+                        // 閾値を超えたらタイムアウト(停止)扱いとする
+                        _stopped = true;
+                        // 停止通知
+                        onTimeCodeStopped.Invoke();
+
+                        if (debugLog)
+                        {
+                            Debug.Log("LTC signal lost.");
+                        }
+                    }
+                }
+
                 return;
             }
 
+            // 信号受信中
             TimeCode = timeCode;
             TimeSeconds = timeSeconds;
-
+            // デコード結果を通知
             onTimeCodeUpdated.Invoke(TimeCode, TimeSeconds);
+
+            // タイムアウト扱いの諸々を消し飛ばす
+            _stopped = false;
+            _timeoutTimer = 0f;
         }
     }
 }
